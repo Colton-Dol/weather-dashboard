d9fbb2250a4144ea2deff3a7379d2010caca40fd
Colton Doling <cmdoling@gmail.com>
2024-11-17 15:16:51 -0500

    imported both weather and history service from the service folder

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,6 +1,7 @@
 import { Router, type Request, type Response } from 'express';
 const router = Router();
-
+import  HistoryService from '../../service/historyService.js';
+import WeatherService from '../../service/weatherService.js';
 // import HistoryService from '../../service/historyService.js';
 // import WeatherService from '../../service/weatherService.js';
 
