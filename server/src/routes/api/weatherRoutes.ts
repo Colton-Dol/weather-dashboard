f77175fd4f700ae95c563fe826bf1b51c0276379
Colton Doling <cmdoling@gmail.com>
2024-11-24 17:14:17 -0500

    added get request to weatherRoutes

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -14,14 +14,21 @@ router.post('/', (req: Request, res: Response) => {
       res.json(data)
      })
   } catch (error) {
-    res.send(error)
+    console.log(error);
   }
   // TODO: GET weather data from city name
   // TODO: save city to search history
 });
 
 // TODO: GET search history
-router.get('/history', async (_req: Request, _res: Response) => {});
+router.get('/history', async (_req: Request, res: Response) => {
+  try {
+     const savedCities = HistoryService.getCities();
+     res.json(savedCities)
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // * BONUS TODO: DELETE city from search history
 // router.delete('/history/:id', async (req: Request, res: Response) => {});
