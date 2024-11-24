190b0ef50f074f87d7bbda11ffd4c572ae09ee1a
Colton Doling <cmdoling@gmail.com>
2024-11-24 17:10:51 -0500

    added the POST request to weatherRoutes

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -7,14 +7,23 @@ import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', (req: Request, res: Response) => {
+  try {
+     const cityName = req.body.cityName
+     WeatherService.getWeatherForCity(cityName).then((data)=>{
+      HistoryService.addCity(cityName)
+      res.json(data)
+     })
+  } catch (error) {
+    res.send(error)
+  }
   // TODO: GET weather data from city name
   // TODO: save city to search history
 });
 
 // TODO: GET search history
-router.get('/history', async (req: Request, res: Response) => {});
+router.get('/history', async (_req: Request, _res: Response) => {});
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (req: Request, res: Response) => {});
+// router.delete('/history/:id', async (req: Request, res: Response) => {});
 
 export default router;
